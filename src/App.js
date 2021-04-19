9fc15a0ae0de8992fa22b63c880c31b9305fc8fc
sjaak1992 <syakirahalgadri@gmail.com>
2021-04-19 18:27:51 +0200

    added api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [title, setTitle] = useState([]);
+    const [author, setAuthor] = useState([])
+    const [description, setDescription] = useState([])
+
+
+    useEffect(() =>{
+        async function fetchData() {
+            const response = await axios.get("https://openlibrary.org/authors/OL34184A.json")
+            // console.log(response.data)
+            setAuthor(response.data.name)
+        }
+
+        fetchData();
+
+    },[])
+
+
+    useEffect(() =>{
+        async function fetchData() {
+            const response = await axios.get("https://openlibrary.org/works/OL45883W.json")
+            // console.log(response.data)
+            setTitle(response.data.title)
+            setDescription(response.data.description)
+        }
+
+        fetchData();
+
+    },[])
+
+
+
+
+    return (
+        <>
+            <div className="App">
+             <h1>Book nr. One</h1>
+                <h2>{title}</h2>
+                <h3>{author}</h3>
+                <p>{description}</p>
+
+
+            </div>
+        </>
+
+    );
 }
 
 export default App;
