a546068bff9995cd6cdaed91913dd50ca484fe18
sjaak1992 <syakirahalgadri@gmail.com>
2021-04-20 16:34:33 +0200

    added correct api and returned book list

--- src/App.js
+++ src/App.js
@@ -3,48 +3,48 @@ import './App.css';
 import axios from "axios";
 
 function App() {
-
-
-    const [title, setTitle] = useState([]);
+    const [query, setQuery] = useState('dahl')
     const [author, setAuthor] = useState([])
-    const [description, setDescription] = useState([])
+    const [books, setBooks] = useState([]);
 
-
-    useEffect(() =>{
+    useEffect(() => {
         async function fetchData() {
-            const response = await axios.get("https://openlibrary.org/authors/OL34184A.json")
-            // console.log(response.data)
-            setAuthor(response.data.name)
+            const response = await axios.get(`http://openlibrary.org/search.json?author=${query}`)
+            console.log(response.data.docs)
+
+            if (response.data.docs) {
+                setAuthor(response.data.docs[0].author_name[0])
+                // setBooks(response.data.docs[0].title)
+                setBooks(response.data.docs)
+            }
+
         }
 
         fetchData();
 
-    },[])
+    }, [query])
 
 
-    useEffect(() =>{
-        async function fetchData() {
-            const response = await axios.get("https://openlibrary.org/works/OL45883W.json")
-            // console.log(response.data)
-            setTitle(response.data.title)
-            setDescription(response.data.description)
-        }
-
-        fetchData();
-
-    },[])
-
-
+    async function fetchData(event) {
+        const response = await axios.get(`http://openlibrary.org/search.json?author=${query}`)
+        setAuthor(response.data.docs[0].author_name[0])
+    }
 
 
     return (
         <>
             <div className="App">
-             <h1>Book nr. One</h1>
-                <h2>{title}</h2>
-                <h3>{author}</h3>
-                <p>{description}</p>
+                <h1>Books of my favourite author: </h1>
+                <input value={query} onChange={e => setQuery(e.target.value)} type="search"/>
+                <button onClick={fetchData}> Search</button>
+            </div>
 
+            <div>
+                <p>{author && author}</p>
+                <p>{books && books.map((book) => {
+                    console.log(book)
+                    return<p>{book.title}</p>
+                })}</p>
 
             </div>
         </>
