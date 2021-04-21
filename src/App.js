3ad132b7f380d31e98cd5a478b3c7fa2f7ff70f1
sjaak1992 <syakirahalgadri@gmail.com>
2021-04-21 12:28:47 +0200

    fetch books by author, add to readinglist

--- src/App.js
+++ src/App.js
@@ -1,55 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import './App.css';
-import axios from "axios";
+import Booklist from "./Components/Booklist";
 
 function App() {
-    const [query, setQuery] = useState('dahl')
-    const [author, setAuthor] = useState([])
-    const [books, setBooks] = useState([]);
+    return(
 
-    useEffect(() => {
-        async function fetchData() {
-            const response = await axios.get(`http://openlibrary.org/search.json?author=${query}`)
-            console.log(response.data.docs)
+    <Booklist />
 
-            if (response.data.docs) {
-                setAuthor(response.data.docs[0].author_name[0])
-                // setBooks(response.data.docs[0].title)
-                setBooks(response.data.docs)
-            }
+);
 
-        }
-
-        fetchData();
-
-    }, [query])
-
-
-    async function fetchData(event) {
-        const response = await axios.get(`http://openlibrary.org/search.json?author=${query}`)
-        setAuthor(response.data.docs[0].author_name[0])
-    }
-
-
-    return (
-        <>
-            <div className="App">
-                <h1>Books of my favourite author: </h1>
-                <input value={query} onChange={e => setQuery(e.target.value)} type="search"/>
-                <button onClick={fetchData}> Search</button>
-            </div>
-
-            <div>
-                <p>{author && author}</p>
-                <p>{books && books.map((book) => {
-                    console.log(book)
-                    return<p>{book.title}</p>
-                })}</p>
-
-            </div>
-        </>
-
-    );
 }
 
 export default App;
