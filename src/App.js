cd09345d25651115b6cb0524f6e1ef49667fa35e
sjaak1992 <syakirahalgadri@gmail.com>
2021-04-28 10:35:45 +0200

    added styles searchbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import BookCard from "./Components/BookCard";
 import MyReadingList from "./Components/MyReadingList";
 import Search from "./Components/Search";
+import search_image from "./assets/search_image.jpg";
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
     const [readingList, setReadingList] = useState([])
     const [authorProfile, setAuthorProfile] = useState([])
 
-    const fetchData = useCallback (async function fetchData() {
+    const fetchData = useCallback(async function fetchData() {
         const response = await axios.get(`http://openlibrary.org/search.json?author=${query}`)
         console.log(response)
 
@@ -36,57 +37,72 @@ function App() {
 
     return (
         <>
-            <div className="App">
-                <h2>Books of my favourite author: </h2>
-                <h2>{author && author}</h2>
-                <img src={`http://covers.openlibrary.org/a/olid/${authorProfile}-M.jpg`} alt="author-profile"/>
+
+            <div className="app-container">
+
+                <div className="app-container-left">
+                    <h1 className="logo">THE BOOKCLUB</h1>
+                </div>
+                <div className="app-container-right">
+
+                </div>
 
 
+                <div className="App">
+                    <Search data={fetchData}
+                            value={query}
+                            change={e => setQuery(e.target.value)}
+                            image={search_image}
+                            alternative="search-image"
+                            authorProfile={authorProfile}
+
+                    />
+
+                    <h2>{author && author}</h2>
+
+
+
+
+                </div>
+
+
+
+
+
+                {/*<div className="my-reading-list">*/}
+
+                {/*    <ul>*/}
+                {/*        {readingList.map((title) => {*/}
+                {/*            return (*/}
+
+                {/*                <MyReadingList item={title} />*/}
+
+                {/*            )*/}
+                {/*        })}*/}
+                {/*    </ul>*/}
+
+                {/*</div>*/}
+
+
+                {/*<div className="book-card-container">*/}
+
+                {/*    <p>*/}
+                {/*        {books && books.map((book) => {*/}
+                {/*        // console.log("HIER ZIT OOK EEN LOGJE", book)*/}
+
+                {/*        return (*/}
+                {/*                <BookCard data={() => setReadingList([...readingList, book.title])}*/}
+                {/*                          title={book.title}*/}
+                {/*                          id={book.cover_i}*/}
+                {/*                          firstline={book.first_sentence}*/}
+                {/*                          author={book.author_key}*/}
+                {/*                />*/}
+                {/*        )*/}
+                {/*    })}*/}
+                {/*    </p>*/}
+
+                {/*</div>*/}
             </div>
-
-            <div className="search">
-
-                <Search data={fetchData}
-                        value={query}
-                        change={e => setQuery(e.target.value)} />
-
-            </div>
-
-
-            <div className="my-reading-list">
-
-                <ul>
-                    {readingList.map((title) => {
-                        return (
-
-                            <MyReadingList item={title} />
-
-                        )
-                    })}
-                </ul>
-
-            </div>
-
-
-            <div className="book-card-container">
-
-                <p>
-                    {books && books.map((book) => {
-                    // console.log("HIER ZIT OOK EEN LOGJE", book)
-
-                    return (
-                            <BookCard data={() => setReadingList([...readingList, book.title])}
-                                      title={book.title}
-                                      id={book.cover_i}
-                                      firstline={book.first_sentence}
-                                      author={book.author_key}
-                            />
-                    )
-                })}
-                </p>
-
-            </div>
-
         </>
 
     );
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,21 +1,41 @@
 import React from "react";
 import './Search.css'
+import {FcSearch} from 'react-icons/fc'
 
-function Search (props){
+function Search(props) {
     return (
         <>
-            <div id="container">
-                <div className="container-left">
+            <div className="container">
 
-        <input value={props.value} onChange={props.change}/>
-        <button onClick={props.data} >Search</button>
+                <div className="left">
+
+                    <div>
+                    <h2>Find Books</h2>
+                    <h2>By My Favorite Author.</h2>
+                    </div>
+
+                    <div>
+                    <input value={props.value} onChange={props.change}/>
+                    <button onClick={props.data}><FcSearch/></button>
+                    </div>
+
                 </div>
 
 
-                <div className="container-right"></div>
+                <div className="right">
+
+                    <div className="author-picture">
+                        <img src={`http://covers.openlibrary.org/a/olid/${props.authorProfile}-M.jpg`}
+                             alt="author-profile"/>
+                    </div>
+
+                    <img className="search-image" src={props.image} alt={props.alternative}/>
+
+                </div>
             </div>
+
         </>
-            )
+    )
 }
 
 export default Search;
