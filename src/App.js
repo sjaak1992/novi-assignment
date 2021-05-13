d997457651057ac63968ee11376fa71210d06776
sjaak1992 <syakirahalgadri@gmail.com>
2021-05-13 14:33:29 +0200

    added private routing for reading list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import {Switch, Route} from "react-router-dom";
+import {Switch, Route, Redirect} from "react-router-dom";
 import './App.css';
 import Nav from "./Components/Nav"
 import ReadingListPage from "./Pages/readinglist-page/ReadingListPage";
 import LoginPage from "./Pages/login-page/LoginPage";
 import SearchPage from "./Pages/search-page/SearchPage";
 import LogoutPage from "./Pages/logout-page/LogoutPage";
+import {useAuth} from "./Contexts/AuthContext";
+
+
+
 
 
 function App() {
+    const {appUser} = useAuth()
+    console.log("user?", appUser)
     return (
         <>
 
@@ -22,7 +28,8 @@ function App() {
                         </Route>
 
                         <Route path="/reading-list">
-                            <ReadingListPage/>
+                            {appUser ? <ReadingListPage/> : <Redirect to="/login"/>}
+
                         </Route>
 
 
